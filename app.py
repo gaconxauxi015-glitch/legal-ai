1c12130f8cc310ca3a805005c125de6a8b7e27df
gaconxauxi015-glitch <gaconxauxi015@gmail.com>
2026-05-09 14:35:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+
+import streamlit as st
+import google.generativeai as genai
+from PyPDF2 import PdfReader
+from docx import Document
+import os
+
+# API KEY
+genai.configure(
+    api_key="DAN_API_KEY_CUA_BAN"
+)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+st.set_page_config(page_title="Legal AI Assistant")
+
+st.title("⚖️ Legal AI Assistant")
+
+st.write("Trợ lý AI pháp lý và hợp đồng")
+
+uploaded_file = st.file_uploader(
+    "Upload hợp đồng PDF hoặc Word",
+    type=["pdf", "docx"]
+)
+
+document_text = ""
+
+if uploaded_file:
+
+    if uploaded_file.name.endswith(".pdf"):
+
+        pdf = PdfReader(uploaded_file)
+
+        for page in pdf.pages:
+            document_text += page.extract_text()
+
+    elif uploaded_file.name.endswith(".docx"):
+
+        doc = Document(uploaded_file)
+
+        for para in doc.paragraphs:
+            document_text += para.text + "\n"
+
+    st.success("Đã tải tài liệu")
+
+user_input = st.text_area(
+    "Nhập yêu cầu pháp lý"
+)
+
+if st.button("Phân tích"):
+
+    prompt = f"""
+    Bạn là chuyên gia pháp lý lao động tại Việt Nam.
+
+    Tài liệu:
+    {document_text}
+
+    Yêu cầu:
+    {user_input}
+
+    Hãy:
+    - phân tích rủi ro
+    - tìm điều khoản bất lợi
+    - đề xuất chỉnh sửa
+    - trả lời dễ hiểu
+    """
+
+    response = model.generate_content(prompt)
+
+    st.write(response.text)
